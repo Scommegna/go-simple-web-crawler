0994cc464b6cf6c2e3d0fc1fd7dd88ce2a254ebb
Scommegna <scommegnal@hotmail.com>
2026-02-21 17:31:21 -0300

    feat: created set data structure to get only unique links

--- main/links.go
+++ main/links.go
@@ -6,12 +6,16 @@ import (
 
 func GetLinks(body string) []string {
 	var links []string
+	set := make(map[string]struct{})
 
 	re := regexp.MustCompile(`href="(https?://[^"]*)"`)
 	matches := re.FindAllStringSubmatch(body, -1)
 
 	for _, match := range matches {
-		links = append(links, match[1])
+		if _, exists := set[match[1]]; !exists {
+			links = append(links, match[1])
+			set[match[1]] = struct{}{}
+		}
 	}
 
 	return links	
